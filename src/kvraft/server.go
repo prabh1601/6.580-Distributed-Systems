756d00c08413aaada597d1f318fa49f568180f88
prabh <prabh1601@gmail.com>
2024-04-28 20:29:33 +0530

    3B | Add basic snapshot handling - too many bugs

--- src/kvraft/server.go
+++ src/kvraft/server.go
@@ -11,41 +11,56 @@ import (
 	"sync/atomic"
 )
 
-type AckManager struct {
-	waitCh   *haxmap.Map[string, *chan OpState]
-	ackStore *haxmap.Map[string, OpState] // stores status of an ongoing/completed operation
+type Store struct {
+	waitCh   *haxmap.Map[string, *chan OpState] // stores wait channels for goroutines waiting on quorum
+	ackStore *haxmap.Map[string, OpState]       // stores status of an ongoing/completed operation
+	kvStore  *haxmap.Map[string, string]        // key value pair store
 }
 
-type KVServer struct {
-	utils.Logger
-	AckManager
-	kvStore      *haxmap.Map[string, string] // key value pair store
-	me           int
-	rf           *raft.Raft
-	applyCh      chan raft.ApplyMsg
-	dead         int32 // set by Kill()
-	maxraftstate int   // snapshot if log grows this big
+func (st *Store) getValue(key string) string {
+	value, exists := st.kvStore.Get(key)
+	if !exists {
+		value = ""
+	}
+	return value
 }
 
-// return waitCh, existing
-func (am *AckManager) getOrCreateWaitObject(ackKey string) (*chan OpState, bool) {
+func (st *Store) getKvStore() *haxmap.Map[string, string] {
+	return st.kvStore
+}
+
+func (st *Store) getAckStore() *haxmap.Map[string, OpState] {
+	return st.ackStore
+}
+
+func (st *Store) setValue(key, value string) {
+	st.kvStore.Set(key, value)
+}
+
+func (st *Store) getAckStage(ackKey string) (OpState, bool) {
+	return st.ackStore.Get(ackKey)
+}
+
+func (st *Store) getOrCreateWaitChan(ackKey string) *chan OpState {
 	waitCh := make(chan OpState, 1)
-	return am.waitCh.GetOrSet(ackKey, &waitCh)
+	ch, _ := st.waitCh.GetOrSet(ackKey, &waitCh)
+	return ch
 }
 
-func (am *AckManager) cleanRequestMetadata(ackKey string) {
-	am.ackStore.Del(ackKey)
-	am.waitCh.Del(ackKey)
+func (st *Store) cleanRequest(ackKey string) {
+	st.ackStore.Del(ackKey)
+	st.waitCh.Del(ackKey)
 }
 
-func (am *AckManager) createRequestMetadata(ackKey string) {
-	am.getOrCreateWaitObject(ackKey) // create wait object for this key
-	am.ackStore.Set(ackKey, STARTED) // mark started
+func (st *Store) createRequest(ackKey string) {
+	// order matters here as we first create wait chan and then mark it as started
+	st.getOrCreateWaitChan(ackKey)   // create wait object for this key
+	st.ackStore.Set(ackKey, STARTED) // mark started
 }
 
-func (am *AckManager) completeRequestMetadata(ackKey string) bool {
-	am.ackStore.Set(ackKey, COMPLETED) // mark completed
-	waitCh, _ := am.getOrCreateWaitObject(ackKey)
+func (st *Store) completeRequest(ackKey string) bool {
+	st.ackStore.Set(ackKey, COMPLETED) // mark completed
+	waitCh := st.getOrCreateWaitChan(ackKey)
 	// this will not block if previous ack is not already consumed
 	select {
 	case *waitCh <- COMPLETED:
@@ -55,14 +70,44 @@ func (am *AckManager) completeRequestMetadata(ackKey string) bool {
 	}
 }
 
-func (am *AckManager) abortRequestMetadata(ackKey string) bool {
-	aborted := am.ackStore.CompareAndSwap(ackKey, STARTED, ABORTED) // mark aborted
+func (st *Store) abortRequest(ackKey string) bool {
+	aborted := st.ackStore.CompareAndSwap(ackKey, STARTED, ABORTED) // mark aborted
 	if aborted {
-		waitCh, _ := am.getOrCreateWaitObject(ackKey)
+		waitCh := st.getOrCreateWaitChan(ackKey)
 		*waitCh <- ABORTED
 	}
 
 	return aborted
+}
+
+type KVServer struct {
+	utils.Logger
+	Store
+	me             int
+	rf             *raft.Raft
+	applyCh        chan raft.ApplyMsg
+	dead           int32 // set by Kill()
+	maxRaftState   int   // snapshot if log grows this big
+	lastAppliedIdx int   // raftLog index of last applied ApplyMsg
+}
+
+func (kv *KVServer) MakeStore(kvStore *haxmap.Map[string, string], ackStore *haxmap.Map[string, OpState], waitChan *haxmap.Map[string, *chan OpState]) {
+	kv.Store = Store{
+		kvStore:  kvStore,
+		ackStore: ackStore,
+		waitCh:   waitChan,
+	}
+}
+
+func (kv *KVServer) getLastAppliedIdx() int {
+	return kv.lastAppliedIdx
+}
+
+func (kv *KVServer) setLastAppliedIdx(newIdx int) {
+	if newIdx <= kv.lastAppliedIdx {
+		kv.LogPanic("Reapplied already applied idx:", newIdx, "over lastApplied idx:", kv.lastAppliedIdx)
+	}
+	kv.lastAppliedIdx = newIdx
 }
 
 func (kv *KVServer) getAckKey(clientId, opId int64) string {
@@ -80,9 +125,9 @@ func (kv *KVServer) getPreviousAckKey(key string) string {
 
 func (kv *KVServer) convertToRaftCommandForPut(args *PutAppendArgs) RaftCommand {
 	return RaftCommand{
+		OpType:   args.Op,
 		OpId:     args.OpId,
 		ClientId: args.ClientId,
-		OpType:   args.Op,
 		Key:      args.Key,
 		Value:    args.Value,
 	}
@@ -104,12 +149,7 @@ func (kv *KVServer) HandleGet(args *GetArgs, reply *GetReply) {
 	reply.Err = err
 	reply.LeaderId = kv.rf.GetLeaderPeerIndex()
 	if err == OK {
-		value, exists := kv.kvStore.Get(args.Key)
-		if !exists {
-			reply.Err = NO_KEY
-		} else {
-			reply.Value = value
-		}
+		reply.Value = kv.getValue(args.Key)
 	}
 }
 
@@ -124,7 +164,7 @@ func (kv *KVServer) HandlePutAppend(args *PutAppendArgs, reply *PutAppendReply) 
 func (kv *KVServer) startQuorum(command RaftCommand) (bool, Err) {
 	ackKey := kv.getAckKey(command.ClientId, command.OpId)
 	// check if this command is already ack-ed
-	if stage, ackExists := kv.ackStore.Get(ackKey); ackExists && stage == COMPLETED {
+	if stage, ackExists := kv.getAckStage(ackKey); ackExists && stage == COMPLETED {
 		return true, OK
 	}
 
@@ -134,7 +174,7 @@ func (kv *KVServer) startQuorum(command RaftCommand) (bool, Err) {
 		return false, WRONG_LEADER
 	}
 
-	kv.createRequestMetadata(ackKey)
+	kv.createRequest(ackKey)
 	kv.LogInfo("Initiated quorum for", *(&command), "submitted at index:", index)
 	return kv.finishQuorum(ackKey, term)
 }
@@ -143,11 +183,11 @@ func (kv *KVServer) finishQuorum(ackKey string, quorumTerm int) (bool, Err) {
 	kv.LogDebug("Started wait for quorum on key:", ackKey)
 
 	currentTerm, _ := kv.rf.GetStatus()
-	if quorumTerm != currentTerm && kv.abortRequestMetadata(ackKey) {
+	if quorumTerm != currentTerm && kv.abortRequest(ackKey) {
 		kv.LogDebug("Aborting quorum on key:", ackKey)
 	}
 
-	waitCh, _ := kv.getOrCreateWaitObject(ackKey)
+	waitCh := kv.getOrCreateWaitChan(ackKey)
 	operationStage := <-*waitCh
 	kv.LogDebug("Finished wait for quorum on key:", ackKey)
 	if operationStage != COMPLETED {
@@ -157,53 +197,114 @@ func (kv *KVServer) finishQuorum(ackKey string, quorumTerm int) (bool, Err) {
 	return true, OK
 }
 
+func (kv *KVServer) shouldSnapshot() bool {
+	return kv.maxRaftState != -1 && kv.rf.HasReachedSizeThreshold(kv.maxRaftState)
+}
+
+func (kv *KVServer) triggerSnapshot() bool {
+	snapshot := utils.IntToBytes(kv.getLastAppliedIdx())
+
+	if kvStoreBytes, err := kv.getKvStore().MarshalJSON(); err != nil {
+		kv.LogPanic("Failed to serialize current kvStore", err)
+	} else {
+		snapshot = append(snapshot, utils.IntToBytes(len(kvStoreBytes))...)
+		snapshot = append(snapshot, kvStoreBytes...)
+	}
+
+	if ackStoreBytes, err := kv.getAckStore().MarshalJSON(); err != nil {
+		kv.LogPanic("Failed to serialize current ackStore", err)
+	} else {
+		snapshot = append(snapshot, ackStoreBytes...)
+	}
+
+	kv.LogInfo("Triggering snapshot with kvStore till index", kv.getLastAppliedIdx())
+	println()
+	return kv.rf.Snapshot(kv.getLastAppliedIdx(), snapshot)
+}
+
+func (kv *KVServer) processSnapshot(msg raft.ApplyMsg) {
+	snapshotBytes := msg.Snapshot
+	intSize := 8
+
+	snapshotIdx := utils.BytesToInt(snapshotBytes[:intSize])
+	if snapshotIdx < kv.getLastAppliedIdx() {
+		kv.LogError("Trying to install state snapshot till index:", snapshotIdx, "whereas RSM has applied indexes upto:", kv.getLastAppliedIdx())
+		return
+	}
+
+	kvStoreSize := utils.BytesToInt(snapshotBytes[intSize : 2*intSize])
+	kv.LogInfo("Applying snapshot till index:", snapshotIdx)
+
+	kvStoreBytes := snapshotBytes[2*intSize : (2*intSize)+kvStoreSize]
+	kvStore := haxmap.New[string, string]()
+	if err := kvStore.UnmarshalJSON(kvStoreBytes); err != nil {
+		kv.LogPanic("Failed to deserialize kvStore", err)
+	}
+
+	ackStoreBytes := snapshotBytes[(2*intSize)+kvStoreSize:]
+	ackStore := haxmap.New[string, OpState]()
+	if err := ackStore.UnmarshalJSON(ackStoreBytes); err != nil {
+		kv.LogPanic("Failed to deserialize ackStore", err)
+	}
+
+	waitCh := haxmap.New[string, *chan OpState]()
+	ackStore.ForEach(func(ackKey string, state OpState) bool {
+		if state != STARTED {
+			waitChan := make(chan OpState, 1)
+			waitChan <- state
+			waitCh.Set(ackKey, &waitChan)
+		}
+		return true
+	})
+
+	kv.MakeStore(kvStore, ackStore, waitCh)
+}
+
+func (kv *KVServer) processCommand(msg raft.ApplyMsg) {
+	command := msg.Command.(RaftCommand)
+	ackKey := kv.getAckKey(command.ClientId, command.OpId)
+
+	if stage, exists := kv.getAckStage(ackKey); exists && stage == COMPLETED {
+		kv.LogWarn("Skipping completed command :", command)
+	} else {
+		kv.LogDebug("Applying Command to state Machine. Msg :", command)
+		switch command.OpType {
+		case PUT:
+			kv.setValue(command.Key, command.Value)
+		case APPEND:
+			value := kv.getValue(command.Key)
+			value += command.Value
+			kv.setValue(command.Key, value)
+		case GET:
+			// do nothing
+		}
+	}
+
+	if !kv.completeRequest(ackKey) { // complete current request
+		kv.LogDebug("Failed to complete key:", ackKey, "as previous ack event is not consumed yet")
+	}
+
+	kv.cleanRequest(kv.getPreviousAckKey(ackKey)) // clean up previous request meta data from same client
+}
+
 func (kv *KVServer) processCommittedMsg() {
 
 	for kv.killed() == false {
 		applyMsg := <-kv.applyCh
 
-		command := applyMsg.Command.(RaftCommand)
-		ackKey := kv.getAckKey(command.ClientId, command.OpId)
-		if stage, exists := kv.ackStore.Get(ackKey); exists && stage == COMPLETED {
-			kv.LogWarn("Skipping completed command :", applyMsg)
-		} else {
-			kv.LogDebug("Applying Command to state Machine. Msg :", applyMsg)
-
-			if applyMsg.SnapshotValid {
-				kv.processSnapshot(applyMsg)
-			}
-
-			if applyMsg.CommandValid {
-				kv.processCommand(ackKey, command)
-			}
+		if applyMsg.SnapshotValid {
+			kv.processSnapshot(applyMsg)
+			kv.setLastAppliedIdx(applyMsg.SnapshotIndex)
 		}
 
-		if !kv.completeRequestMetadata(ackKey) { // complete current request
-			kv.LogDebug("Failed to complete key:", ackKey, "as previous ack event is not consumed yet")
-
+		if applyMsg.CommandValid {
+			kv.processCommand(applyMsg)
+			kv.setLastAppliedIdx(applyMsg.CommandIndex)
 		}
-		kv.cleanRequestMetadata(kv.getPreviousAckKey(ackKey)) // clean up previous request meta data from same client
-	}
-}
 
-func (kv *KVServer) processSnapshot(msg raft.ApplyMsg) {
-
-}
-
-func (kv *KVServer) processCommand(ackKey string, command RaftCommand) {
-	switch command.OpType {
-	case PUT:
-		kv.kvStore.Set(command.Key, command.Value)
-	case APPEND:
-		value, exists := kv.kvStore.Get(command.Key)
-		if !exists {
-			value = command.Value
-		} else {
-			value += command.Value
+		if kv.shouldSnapshot() && !kv.triggerSnapshot() {
+			kv.LogError("Failed to snapshot current state to raft")
 		}
-		kv.kvStore.Set(command.Key, value)
-	case GET:
-		// do nothing
 	}
 }
 
@@ -212,8 +313,8 @@ func (kv *KVServer) listenRaftState() {
 		term := <-kv.rf.ListenTermChanges()
 		kv.LogDebug("Raft term changed to:", term, "Aborting all awaited client request")
 		// term changed : abort all ongoing client requests
-		kv.waitCh.ForEach(func(ackKey string, ch *chan OpState) bool {
-			if kv.abortRequestMetadata(ackKey) {
+		kv.getAckStore().ForEach(func(ackKey string, state OpState) bool {
+			if state == STARTED && kv.abortRequest(ackKey) {
 				kv.LogDebug("Aborted command :", ackKey)
 			}
 			return true
@@ -247,8 +348,8 @@ func (kv *KVServer) killed() bool {
 // the k/v server should kvStore snapshots through the underlying Raft
 // implementation, which should call persister.SaveStateAndSnapshot() to
 // atomically save the Raft state along with the snapshot.
-// the k/v server should snapshot when Raft's saved state exceeds maxraftstate bytes,
-// in order to allow Raft to garbage-collect its log. if maxraftstate is -1,
+// the k/v server should snapshot when Raft's saved state exceeds maxRaftState bytes,
+// in order to allow Raft to garbage-collect its log. if maxRaftState is -1,
 // you don't need to snapshot.
 // StartKVServer() must return quickly, so it should start goroutines
 // for any long-running work.
@@ -259,10 +360,8 @@ func StartKVServer(servers []*labrpc.ClientEnd, me int, persister *raft.Persiste
 
 	kv := new(KVServer)
 	kv.me = me
-	kv.maxraftstate = maxraftstate
-	kv.kvStore = haxmap.New[string, string]()
-	kv.ackStore = haxmap.New[string, OpState]()
-	kv.waitCh = haxmap.New[string, *chan OpState]()
+	kv.maxRaftState = maxraftstate
+	kv.MakeStore(haxmap.New[string, string](), haxmap.New[string, OpState](), haxmap.New[string, *chan OpState]())
 	kv.Logger = utils.GetLogger("server_logLevel", func() string {
 		return "[SERVER] [Peer : " + strconv.Itoa(me) + "] "
 	})
